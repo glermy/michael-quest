213b9188a496480d2d14f407fe1e99e88c26bcb4
glermy <fpsfarooqh75@stu.fpsk12.net>
2025-12-08 18:30:15 -0500

    goal flags

---
+++ goal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+@export var level_name: String
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://"+level_name)
+		
